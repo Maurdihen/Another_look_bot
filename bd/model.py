8280de3c906790b988e24e915cbe487a73976cef
egorarhipov <arhipov_0406@mail.ru>
2023-07-25 22:30:37 +0300

    add bd

---
+++ bd/model.py
@@ -0,0 +1,36 @@
+from sqlalchemy import Column, Integer, String, DateTime, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+class Users(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer)
+    name = Column(String)
+
+
+class Notes(Base):
+    __tablename__ = "notes"
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    date = Column(DateTime)
+    time = Column(String)
+    category = Column(String)
+    sub_category = Column(String)
+
+    user = relationship("Users", back_populates="notes")
+
+
+Users.notes = relationship("Notes", back_populates="user")
+
+engine = create_engine('sqlite:///notes.db')
+Base.metadata.create_all(engine)
+
+Session = sessionmaker(bind=engine)
+session = Session()
