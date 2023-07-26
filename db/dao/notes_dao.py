b68490f23dd3afd85835dfaa0152c84395e68acc
egorarhipov <arhipov_0406@mail.ru>
2023-07-26 18:38:29 +0300

    add CRUD

--- db/dao/notes_dao.py
+++ db/dao/notes_dao.py
@@ -16,3 +16,25 @@ class NotesDAO:
         self.session.add(new_note)
         self.session.commit()
         return new_note
+
+    def get_notes_by_user_id(self, user_id: int) -> list[Notes]:
+        return self.session.query(Notes).filter_by(user_id=user_id).all()
+
+    def get_note_by_id(self, note_id: int) -> Notes:
+        return self.session.query(Notes).filter_by(id=note_id).first()
+
+    def update_note(self, note_id: int, date: str, time: str, category: str, sub_category: str) -> Notes:
+        note = self.get_note_by_id(note_id)
+        if note:
+            note.date = date
+            note.time = time
+            note.category = category
+            note.sub_category = sub_category
+            self.session.commit()
+            return note
+
+    def delete_note(self, note_id: int) -> None:
+        note = self.get_note_by_id(note_id)
+        if note:
+            self.session.delete(note)
+            self.session.commit()
