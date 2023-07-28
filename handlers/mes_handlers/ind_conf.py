95ff5831bfeed5bdd1f5382c7280bd26e9a8d7ac
Egor Arhipov <111359850+Maurdihen@users.noreply.github.com>
2023-07-29 01:49:23 +0300

    connect api

---
+++ handlers/mes_handlers/ind_conf.py
@@ -0,0 +1,35 @@
+from aiogram.dispatcher import FSMContext
+from aiogram.types import Message
+
+from bot_tg.loader import dp, bot
+from buttons.reply import number
+from states import UserStates
+from calendar_api.main import Calendar
+
+
+@dp.message_handler(state=UserStates.GetNumber)
+async def get_name_number(message: Message, state: FSMContext):
+    text = message.text
+    await state.update_data(text=text)
+    await bot.send_message(message.chat.id, text="И остался последний шаг, поделитесь своим номером телефона, "
+                                                 "чтобы психолог смог с вами связаться, для этого "
+                                                 "нажмите на кнопку ниже",
+                           reply_markup=number
+                           )
+
+
+@dp.message_handler(content_types=['contact'], state='*')
+async def my_number(message: Message, state: FSMContext):
+    from handlers.callbacks.general_info import event, cons
+    async with state.proxy() as data:
+        text = data.get("text")
+    await bot.send_message(message.chat.id, text=(message.contact["phone_number"], text, event))
+    date = {
+        "summary": cons,
+        "name": text,
+        "phone_number": message.contact["phone_number"],
+        "start": f"{event['date']['year']}-{event['date']['month']}-{event['date']['day']}T{event['startTime']}+03:00",
+        "end": f"{event['date']['year']}-{event['date']['month']}-{event['date']['day']}T{event['endTime']}+03:00",
+    }
+    Calendar.create_calendar_event(date)
+    await state.finish()
