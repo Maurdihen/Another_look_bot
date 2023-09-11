c30125a64d73ffe02f67a834332c8aabe826e2eb
vvvadim <v.vladimirov@it-park.tech>
2023-09-11 18:48:33 +0300

    fixed models

--- db_work/crud/reqs/event.py
+++ db_work/crud/reqs/event.py
@@ -1,10 +1,11 @@
 class EventRequest:
 
-    def __init__(self, tg_id, event_service):
+    def __init__(self, tg_id, event_service, user_service):
         self.event_service = event_service
+        self._user = user_service.get_user_by_tg_id(tg_id)
 
     def get_events(self):
-        return self.user.events
+        return self._user.events
 
     def book_event(self, data):
         return self.event_service.update_event(data)
