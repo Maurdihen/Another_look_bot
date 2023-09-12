b074e78e5848a1a87eb78446ea034379391d6549
vvvadim <v.vladimirov@it-park.tech>
2023-09-13 00:17:59 +0300

    finally working many-to-many

--- db_work/crud/reqs/user.py
+++ db_work/crud/reqs/user.py
@@ -1,21 +1,27 @@
+from db_work.crud.implemented import user_service
+
+
 class UserRequest:
-    def __init__(self, tg_id, user_service, event_service):
-        self.user_service = user_service
-        self.event_service = event_service
-        self.tg_id = tg_id
-        self._user = self.user_service.get_user_by_tg_id(self.tg_id)
+    def __init__(self, tg_id):
+        self._user_service = user_service
+        self._tg_id = tg_id
+        self._user = self._user_service.get_user_by_tg_id(self._tg_id)
 
         if self._user is None:
             self._user = self._create_new_user()
 
+    @property
+    def tg_id(self):
+        return self._tg_id
+
     def _create_new_user(self):
         user_data = {
-            "tg_id": self.tg_id,
+            "tg_id": self._tg_id,
             "full_name": None,
             "phone_number": None
         }
 
-        return self.user_service.create_user(user_data)
+        return self._user_service.create_user(user_data)
 
     def get_user_info(self):
         return {
@@ -26,4 +32,4 @@ class UserRequest:
 
     def update_user_info(self, update_data):
         user = self._user
-        self.user_service.update_user(user, update_data)
+        self._user_service.update_user(user, update_data)
